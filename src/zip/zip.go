1642f96aa4825e35e96e48eae4891899014c10ef
Buk Bukowski <pythoncontrol@gmail.com>
2021-03-24 22:09:26 +0100

    add generic zipwalker

--- src/zip/zip.go
+++ src/zip/zip.go
@@ -9,39 +9,91 @@ import (
 	"strings"
 )
 
-// ZipDir creates a zip archive copying files from path in WorkDir.
-// Example:
-// 	zipDir := wd.JoinAbs(workingDir, "testdata", "TestWorkDir_Zip", "zip")
-//	err = Zip(ZipWalkDirFunc(zipDir, "zipdest", zw), zipDir)
-func Zip(src, dst string, w *zip.Writer) error {
-	return filepath.WalkDir(src, ZipWalkDirFunc(src, dst, w))
+type File interface {
+	// Open is responsible for returning io.ReadCloser for a path.
+	// Implementations working on File have to respect a case when
+	// returned readCloser and err are both nil possibly meaning the path can be skipped.
+	Open(path string) (readCloser io.ReadCloser, err error)
+	// Close if responsible for closing readCloser returned by Open.
+	// It has to respect a case when both; readCloser && err are nil.
+	Close(readCloser io.ReadCloser, err error) error
+	// Copy is responsible for copying data into zip writer.
+	// Called between Open and Close only if; Open returned non nil readCloser and nil err.
+	Copy(r io.Reader, w io.Writer) (written int64, err error)
 }
 
-// ZipWalkDirFunc returns fs.WalkDirFunc for zipping files.
-// Files are copied from dst to src in w zip.Writer.
-var ZipWalkDirFunc = func(src, dst string, w *zip.Writer) func(string, fs.DirEntry, error) error {
-	return func(path string, ds fs.DirEntry, err error) error {
 
+// FilePathDenyFunc returns true if path is denied and won't be processed.
+type FilePathDenyFunc func(path string, ds fs.DirEntry, err error) (bool, error)
+
+type FilePathWalker interface {
+	File
+	WalkDir(denyFunc FilePathDenyFunc) error
+}
+
+func NewFilePathWalker(srcDir, dstDir string, w *zip.Writer, perm os.FileMode) FilePathWalker {
+	return &filePathWalker{
+		srcDir:   srcDir,
+		dstDir:   dstDir,
+		writer: w,
+		perm: perm,
+	}
+}
+
+type filePathWalker struct {
+	srcDir string
+	dstDir string
+	writer *zip.Writer
+	perm os.FileMode
+}
+
+func (f filePathWalker) Open(path string) (readCloser io.ReadCloser, err error) {
+	return os.OpenFile(path, os.O_RDONLY, f.perm)
+}
+
+func (f filePathWalker) Close(readCloser io.ReadCloser, err error) error {
+	if readCloser != nil {
+		return readCloser.Close()
+	}
+	return nil
+}
+
+func (f filePathWalker) Copy(r io.Reader, w io.Writer) (written int64, err error) {
+	return io.Copy(w, r)
+}
+
+func (f filePathWalker) WalkDir(denyFunc FilePathDenyFunc) error {
+	return filepath.WalkDir(f.srcDir, func(path string, ds fs.DirEntry, err error) error {
 		// path: /var/www/html/wp-content/plugins/akimset/index.html
-		// src: /var/www/html/wp-content/plugins
-		// dst: /plugins
-		if err != nil {
+		// srcPath: /var/www/html/wp-content/plugins
+		// dstPath: /plugins
+
+		// check for denied path
+		if denyFunc != nil {
+			if denied, err := denyFunc(path, ds, err); err != nil || denied {
+				return err
+			}
+		}
+
+		// open file to be copied into zip archive
+		r, err := f.Open(path)
+		if err != nil || r == nil {
 			return err
 		}
 
 		// path as seen in zip archive
-		zp := filepath.ToSlash(filepath.Join(filepath.Clean(dst), filepath.Clean(path[len(filepath.Clean(src)):])))
+		zipPath := filepath.ToSlash(filepath.Join(filepath.Clean(f.dstDir), filepath.Clean(path[len(filepath.Clean(f.srcDir)):])))
 
 		// handle dir
 		if ds.IsDir() {
 
 			// zip implementation expects "/" if it's a dir
-			if !strings.HasPrefix(zp, "/") {
-				zp += "/"
+			if !strings.HasPrefix(zipPath, "/") {
+				zipPath += "/"
 			}
 
 			// create dir
-			_, err = w.Create(zp)
+			_, err = f.writer.Create(zipPath)
 			if err != nil {
 				return err
 			}
@@ -49,22 +101,13 @@ var ZipWalkDirFunc = func(src, dst string, w *zip.Writer) func(string, fs.DirEnt
 		}
 
 		// create regular file
-		w, err := w.Create(zp)
+		w, err := f.writer.Create(zipPath)
 		if err != nil {
 			return err
 		}
-
-		// open file to copy
-		f, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-
-		defer f.Close()
 
 		// copy file content
-		_, err = io.Copy(w, f)
+		_, err = f.Copy(r, w)
 		return err
-	}
+	})
 }
-
--- src/zip/zip_test.go
+++ src/zip/zip_test.go
@@ -5,76 +5,158 @@ import (
 	"bytes"
 	. "github.com/bukowa/wdgo/src/zip"
 	"io"
-	"log"
 	"os"
 	"path/filepath"
 	"runtime"
 	"testing"
 )
+//
+//func Test_Zip(t *testing.T) {
+//	var testPath = testWorkDir()
+//	log.Print(testPath)
+//
+//	var testDataWant = map[string][]byte{
+//		"zipdest/1":   []byte("11"),
+//		"zipdest/3/4": []byte("44"),
+//		"zipdest/2":   []byte("22"),
+//	}
+//
+//	wd, err := filepath.Abs(testPath)
+//	if err != nil {
+//		t.Error(err)
+//	}
+//
+//	fname := "TestWorkDir_Zip.zip.zip"
+//	zipPath := filepath.Join(wd, "testdata", fname)
+//	//defer os.RemoveAll(zipPath)
+//	os.RemoveAll(zipPath)
+//	f,err := os.OpenFile(zipPath, os.O_CREATE|os.O_EXCL|os.O_RDWR, 0660)
+//	if err != nil {
+//		t.Error(err)
+//	}
+//	var zw = zip.NewWriter(f)
+//
+//	zipDir := filepath.Join(testPath, "testdata", "TestWorkDir_Zip", "zip")
+//	err = Zip(zipDir, "zipdest", zw, nil)
+//	if err != nil {
+//		t.Error(err)
+//		return
+//	}
+//
+//	if err = zw.Close(); err != nil {
+//		t.Error(err)
+//	}
+//
+//	if err = f.Close(); err != nil {
+//		t.Error(err)
+//	}
+//
+//	r, err := zip.OpenReader(zipPath)
+//	if err != nil {
+//		t.Error(err)
+//	}
+//	defer r.Close()
+//
+//	for k, v := range testDataWant {
+//		f, err := r.Open(k)
+//		if err != nil {
+//			t.Error(err)
+//		}
+//		b, err := io.ReadAll(f)
+//		if err != nil {
+//			t.Error(err)
+//		}
+//		if !bytes.Equal(v, b) {
+//			t.Error(v, b)
+//		}
+//	}
+//
+//}
 
-func Test_Zip(t *testing.T) {
-	var testPath = testWorkDir()
-	log.Print(testPath)
-
-	var testDataWant = map[string][]byte{
-		"zipdest/1":   []byte("11"),
-		"zipdest/3/4": []byte("44"),
-		"zipdest/2":   []byte("22"),
-	}
-
-	wd, err := filepath.Abs(testPath)
-	if err != nil {
-		t.Error(err)
-	}
-
-	fname := "TestWorkDir_Zip.zip.zip"
-	zipPath := filepath.Join(wd, "testdata", fname)
-	//defer os.RemoveAll(zipPath)
-	os.RemoveAll(zipPath)
-	f,err := os.OpenFile(zipPath, os.O_CREATE|os.O_EXCL|os.O_RDWR, 0660)
-	if err != nil {
-		t.Error(err)
-	}
-	var zw = zip.NewWriter(f)
-
-	zipDir := filepath.Join(testPath, "testdata", "TestWorkDir_Zip", "zip")
-	err = Zip(zipDir, "zipdest", zw)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	if err = zw.Close(); err != nil {
-		t.Error(err)
-	}
-
-	if err = f.Close(); err != nil {
-		t.Error(err)
-	}
-
-	r, err := zip.OpenReader(zipPath)
-	if err != nil {
-		t.Error(err)
-	}
-	defer r.Close()
-
-	for k, v := range testDataWant {
-		f, err := r.Open(k)
-		if err != nil {
-			t.Error(err)
-		}
-		b, err := io.ReadAll(f)
-		if err != nil {
-			t.Error(err)
-		}
-		if !bytes.Equal(v, b) {
-			t.Error(v, b)
-		}
-	}
-
-}
-
+// testWorkDir returns path of this file.
 func testWorkDir() string {
 	_, filename, _, _ := runtime.Caller(0)
 	return filepath.Dir(filename)
+}
+
+func Test_filePathWalker_WalkDir(t *testing.T) {
+	type fields struct {
+		srcDir string
+		dstDir string
+		writer *zip.Writer
+		perm   os.FileMode
+		want map[string][]byte
+	}
+	type args struct {
+		denyFunc FilePathDenyFunc
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    ".",
+			fields:  fields{
+				srcDir: filepath.Join(testWorkDir(), "testdata", "TestNewFilePathWalker", "zip"),
+				dstDir: "zipdest",
+				// set later in test
+				writer: nil,
+				perm:   0600,
+				want: map[string][]byte{
+					"zipdest/1":   []byte("11"),
+					"zipdest/3/4": []byte("44"),
+					"zipdest/2":   []byte("22"),
+				},
+			},
+			args:    args{
+				denyFunc: nil,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := bytes.NewBuffer(nil)
+			tt.fields.writer = zip.NewWriter(w)
+			f := NewFilePathWalker(
+				tt.fields.srcDir,
+				tt.fields.dstDir,
+				tt.fields.writer,
+				tt.fields.perm)
+			if err := f.WalkDir(tt.args.denyFunc); (err != nil) != tt.wantErr {
+				t.Errorf("WalkDir() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if err := tt.fields.writer.Close(); err != nil {
+				t.Error(err)
+			}
+
+			zipBytes, err := io.ReadAll(w)
+			if err != nil {
+				t.Error(err)
+			}
+
+			r := bytes.NewReader(zipBytes)
+			zReader, err := zip.NewReader(r, r.Size())
+			if err != nil {
+				t.Error(err)
+			}
+
+			// compare
+			for k, v := range tt.fields.want {
+				f, err := zReader.Open(k)
+				if err != nil {
+					t.Error(err)
+				}
+				b, err := io.ReadAll(f)
+				if err != nil {
+					t.Error(err)
+				}
+				if !bytes.Equal(v, b) {
+					t.Error(v, b)
+				}
+			}
+		})
+	}
 }
